ff5955a303950d9320946f9df9f29f5f45e5d1b4
rhymald <rhymald@gmail.com>
2023-09-24 15:15:13 +0200

    Install V

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mag-theta
 P2P gaming server designing, 8th attempt
+
+# V-lang
+
+Install V:
+```bash
+sudo apt-get install gcc make
+git clone https://github.com/vlang/v
+cd v
+make
+sudo ln -s $(pwd)/v /usr/local/bin/v
+```
+## CheatSheets
+
+- `pub` - accessible for other modules
+- `import` - folder+module
+- `..._test.v` - test files
+- `v install module` - install module "module"
+- memory management: -gc / -prealloc / -autofree (default)
---
+++ main.v
@@ -0,0 +1,43 @@
+module main
+
+import json
+
+struct User {
+    name string
+    born  int
+mut:
+    is_registered bool
+}
+
+fn main() {
+    s := '[{"name":"Frodo", "born":25}, {"name":"Bobby", "born":10}]'
+    mut users := json.decode([]User, s) or {
+        eprintln('Failed to parse json')
+        return
+    }
+    for user in users {
+        println('${user.name}: ${user.born}')
+    }
+    println('')
+    for i, mut user in users {
+        println('${i}) ${user.name}')
+        if !user.can_register() {
+            println('Cannot register ${user.name}, they are too young')
+            continue
+        }
+		// `user` is declared as `mut` in the for loop,
+		// modifying it will modify the array
+        user.register()
+    }
+    // Let's encode users again just for fun
+    println('')
+    println(json.encode(users))
+}
+
+fn (u User) can_register() bool {
+    return u.born >= 16
+}
+
+fn (mut u User) register() {
+    u.is_registered = true
+}
