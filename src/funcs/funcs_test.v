887fcb3bc8108f137148fd46dcd0e292f2537481
rhymald <rhymald@gmail.com>
2023-10-15 10:40:55 +0200

    tests

--- src/funcs/funcs_test.v
+++ src/funcs/funcs_test.v
@@ -2,6 +2,7 @@ module funcs
 
 fn test_all() {
 	println('>>---> Start: ${init_epoch()}')
+	print('Random numbers: ')
 	for _ in 0..12 {
 		print(round(init_f64()))
 		init_delay(1000 / 12)
--- src/primitives/primitives_test.v
+++ src/primitives/primitives_test.v
@@ -1,10 +1,12 @@
 module primitives
 
 fn test_all() {
+	print('>>--> List all elements:')
 	for i in -4..8 {
-		print(elem(i)+' ')
+		print(' '+elem(i))
 	}
-	for _ in 0..64 {
+	println('')
+	for _ in 0..16 {
 		println(init_stream().tojson())
 	}
 }
